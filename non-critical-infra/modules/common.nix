a80db7753e6edbff0d6079c18ef23d2ff04a98b7
Gaetan Lepage <gaetan@glepage.com>
2025-09-16 23:39:41 +0200

    users: add gaetan and liberodark

--- non-critical-infra/modules/common.nix
+++ non-critical-infra/modules/common.nix
@@ -35,10 +35,41 @@
   users = {
     mutableUsers = false;
 
-    users.root.openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGQ5hBVVKK72ZX+n+BVnPocx+AG5u6ht8bM++G1lhufp liberodark@gmail.com"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJfOUACs5oAn4Hyt6uMM5e/Xux0/5ODvSeg5zOy4MY1b gaetan@glepage.com"
-    ];
+    users =
+      let
+        sshGaetan = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJfOUACs5oAn4Hyt6uMM5e/Xux0/5ODvSeg5zOy4MY1b gaetan@glepage.com";
+        sshLiberodark = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGQ5hBVVKK72ZX+n+BVnPocx+AG5u6ht8bM++G1lhufp liberodark@gmail.com";
+      in
+      {
+        root.openssh.authorizedKeys.keys = [
+          sshGaetan
+          sshLiberodark
+        ];
+
+        gaetan = {
+          isNormalUser = true;
+          group = "gaetan";
+          extraGroups = [
+            "wheel"
+          ];
+          hashedPassword = "$y$j9T$DcNh44UsDFjthtH7vjQE9/$Nk4ey9oblVpiFwT0zWVgkZdh/WAmR1Kuqz58HwnbBj2";
+          openssh.authorizedKeys.keys = [
+            sshGaetan
+          ];
+        };
+
+        liberodark = {
+          isNormalUser = true;
+          group = "liberodark";
+          extraGroups = [
+            "wheel"
+          ];
+          hashedPassword = "$6$gqUcksN1scoXJMuY$FbgTwt8KBU9/WPWr0jRB32b/XLFCWYxHGwW7qdqmwg9DKn4gWGQtewwZMIjHSSt8H/OAA6bO.cT3wl2QiqD6f0";
+          openssh.authorizedKeys.keys = [
+            sshLiberodark
+          ];
+        };
+      };
 
     #users.nixos = {
     #  isNormalUser = true;
