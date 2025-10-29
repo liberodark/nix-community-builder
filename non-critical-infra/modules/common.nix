a3e4b2f5e49758829babcd67ed98bb988abca24d
Gaetan Lepage <gaetan@glepage.com>
2025-10-29 12:27:23 +0100

    openssh: drop manually set Ciphers/KexAlgorithms/Macs
    This triggers the following warning:
    
    ** WARNING: connection is not using a post-quantum key exchange
    algorithm.
    ** This session may be vulnerable to "store now, decrypt later" attacks.
    ** The server may need to be upgraded. See https://openssh.com/pq.html

--- non-critical-infra/modules/common.nix
+++ non-critical-infra/modules/common.nix
@@ -71,25 +71,6 @@
   services.openssh = {
     enable = true;
     settings = {
-      Ciphers = [
-        "aes256-gcm@openssh.com"
-        "aes128-gcm@openssh.com"
-        "aes256-ctr"
-        "aes128-ctr"
-      ];
-      KexAlgorithms = [
-        "ecdh-sha2-nistp384"
-        "ecdh-sha2-nistp256"
-        "diffie-hellman-group16-sha512"
-        "diffie-hellman-group14-sha256"
-      ];
-      Macs = [
-        "hmac-sha2-512-etm@openssh.com"
-        "hmac-sha2-256-etm@openssh.com"
-        "hmac-sha2-512"
-        "hmac-sha2-256"
-      ];
-
       PermitRootLogin = "yes";
       AllowUsers = [
         "root"
