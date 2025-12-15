51539bf94d17e4cd567df2719eee564fbc38c5ea
liberodark <liberodark@gmail.com>
2025-12-15 11:55:22 +0100

    Improve Shell

--- non-critical-infra/modules/darwin/darwin-common.nix
+++ non-critical-infra/modules/darwin/darwin-common.nix
@@ -36,6 +36,11 @@
   # Fish shell
   programs.fish.enable = true;
 
+  programs.fish.shellInit = ''
+    fish_add_path /run/current-system/sw/bin
+    fish_add_path /nix/var/nix/profiles/default/bin
+  '';
+
   security.sudo.extraConfig = ''
     %admin ALL=(ALL) NOPASSWD: ALL
   '';
