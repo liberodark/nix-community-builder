e3477fbfb6d9234d69112ac6281a332293476dc3
liberodark <liberodark@gmail.com>
2026-06-11 17:31:33 +0200

    Update Kernel (Fix Crypto)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
-    nixos-hardware.url = "github:liberodark/nixos-hardware/461afc574b73abce31abc04fea9ddb0095d3e0c4";
+    nixos-hardware.url = "github:liberodark/nixos-hardware/c46ed2850f480d63dbc1aa6fc040d9acde5eb823";
     # Why?
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
