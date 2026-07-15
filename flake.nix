1d70f95cf4aec6e34c8e26e54c42cabecaee8db3
liberodark <liberodark@gmail.com>
2026-07-15 03:52:22 +0200

    Update Flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
-    nixos-hardware.url = "github:liberodark/nixos-hardware/501e1c22179af5cae034ec4205988e7ea7cb883e";
+    nixos-hardware.url = "github:liberodark/nixos-hardware/3a3b8e7d77321b1591c297028da5a4b06987ab44";
     # Why?
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
