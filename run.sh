c54c80c1a13cc1cd075ef80194958839965670b3
liberodark <liberodark@gmail.com>
2026-09-01 09:23:21 +0200

    Update Flake

--- run.sh
+++ run.sh
@@ -2,8 +2,11 @@
 #nix-shell -p nixos-anywhere --run 'nixos-anywhere -f github:liberodark/nix-community-builder/394ad34#build02.ynh.ovh -L --copy-host-keys root@192.168.0.214 --debug'
 
 #Update
+BUILDER=build02
+COMMIT=c797078
+
 nixos-rebuild switch \
   --no-reexec \
-  --flake github:liberodark/nix-community-builder/45e7824#build02.ynh.ovh \
-  --target-host root@build02.ynh.ovh \
-  --build-host root@build02.ynh.ovh
+  --flake "github:liberodark/nix-community-builder/${COMMIT}#${BUILDER}.ynh.ovh" \
+  --target-host "root@${BUILDER}.ynh.ovh" \
+  --build-host "root@${BUILDER}.ynh.ovh"
